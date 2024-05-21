8c68f5a7460fdde6bd5dfc53274d24d44dcc43dd
coldousedbird <coldousedbird>
2024-05-21 09:32:17 +0300

    updated app structure

---
+++ src/ui/app.py
@@ -0,0 +1,20 @@
+# python src/ui/app.py
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtCore import QFile
+from ui import Ui_MainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
